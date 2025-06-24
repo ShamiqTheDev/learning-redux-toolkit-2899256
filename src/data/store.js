77874bc366a6dee12c94ee2e6e32a498ea6941c7
ShamiqTheDev <64755474+ShamiqTheDev@users.noreply.github.com>
2025-06-24 22:25:42 +0500

    feat: added export in cartSlice, and imported cartSlice in redux store

--- src/data/store.js
+++ src/data/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartSlice from './cartSlice';
 
 
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        cart: cartSlice.reducer
+    }
 });
 
 export default store;
