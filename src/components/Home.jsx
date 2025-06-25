28189f8fb43b07e5f863052b8e363bcd054a1b86
ShamiqTheDev <64755474+ShamiqTheDev@users.noreply.github.com>
2025-06-25 22:25:15 +0500

    feat: Show add/remove cart buttons based on cart state
    Updated Home component to conditionally render 'Add to cart' and 'Remove from cart' buttons based on whether the product is in the cart, using cartProductIds from Redux state.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import productList from '../data/productList.json'
 import cartSlice from '../data/cartSlice'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import '../styles/home.scss'
 
 
 const Home = () => {
 
+  const { cartProductIds } = useSelector((state) => state.cart );
   const { addToCart, removeFromCart } = cartSlice.actions;
   const dispatch = useDispatch();
   
@@ -22,8 +23,11 @@ const Home = () => {
                   <h5 className="card-title">{product.name}</h5>
                   <p className="card-text">${product.price}</p>
 
-                  <button className="btn btn-primary" onClick={() => dispatch(addToCart(product.id))}>Add to cart</button>
-                  <button className="btn btn-secondary" onClick={() => dispatch(removeFromCart(product.id))}>Remove from cart</button>
+                  {!cartProductIds.includes(product.s) && 
+                    <button className="btn btn-primary" onClick={() => dispatch(addToCart(product.id))}>Add to cart</button>}
+                  
+                  {cartProductIds.includes(product.id) && <button className="btn btn-secondary" onClick={() => dispatch(removeFromCart(product.id))}>Remove from cart</button>}
+
                 </div>
               </div>
             </div>
